4801bf000dd43c5e366eac9cac2543a2f555504e
Sophia Kramar <krasofija@yandex.ru>
2019-06-06 11:04:35 +0300

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -1,8 +1,22 @@
+from itertools import tee
+
 def fibo():
      a, b = 0, 1
      while True:           
          yield a            
          a, b = b, a + b
 max_numb = 100
-for index, curr_number in zip(range(max_numb), fibo()):
-      print('{i:3}: {f:3}'.format(i=index+1, f=curr_number))
+for i, f in zip(range(max_numb), fibo()):
+      print('{i:3}: {f:3}'.format(i=i+1, f=f))
+          
+
+def recur_fibo():
+    yield 0
+    yield 1
+    f, tf = tee(recur_fibo()) 
+    next(tf)
+    for a, b in zip(f, tf):
+        yield a + b
+for i, f in zip(range(max_numb), recur_fibo()):
+     print('{i:3}: {f:3}'.format(i=i+1, f=f))
+ 
